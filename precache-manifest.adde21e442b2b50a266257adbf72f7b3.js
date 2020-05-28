f75b5ebcde7c3b681cb9c5ec41c8f5e95d77a0d0
Rishi-Raj-Kalita <rishirajkalita13@gmail.com>
2020-05-28 16:27:30 +0530

    Updates

---
+++ precache-manifest.adde21e442b2b50a266257adbf72f7b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27af3dcef7db4d0a3048339d6e6813b4",
+    "url": "/mobile/index.html"
+  },
+  {
+    "revision": "2d9c3c9ad7929c3fa23f",
+    "url": "/mobile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67102821dbb7c6000b51",
+    "url": "/mobile/static/js/2.69cf9bf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mobile/static/js/2.69cf9bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9c3c9ad7929c3fa23f",
+    "url": "/mobile/static/js/main.aaeb25b5.chunk.js"
+  },
+  {
+    "revision": "982a9ab110cdc058fb03",
+    "url": "/mobile/static/js/runtime-main.1431d30f.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/mobile/static/media/logo.9f7e506d.svg"
+  }
+]);
